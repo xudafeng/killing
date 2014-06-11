ab4a5e7ebc8cee538afb35ee1b2bd59cfdb06e6d
dafeng.xdf <dafeng.xdf@taobao.com>
2014-06-12 01:35:40 +0800

    first commit

---
+++ test/killing.test.js
@@ -0,0 +1,19 @@
+/* ================================================================
+ * killing by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Thu Jun 12 2014 01:24:59 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+"use strict";
+
+var killing = require('../');
+
+describe('', function(){
+});
